de47b8cbfb91f54dc1ded708bbf418786de7e8ab
Chris Mellard <chrism@datacom.co.nz>
2018-09-15 21:41:50 +1200

    Checkin lat/long api

--- Yohan/app.py
+++ Yohan/app.py
@@ -9,46 +9,35 @@ def openDB():
     conn = sqlite3.connect('DCC.db')
     return conn,conn.cursor()
 
-def addLocation(lat, long):
-    conn,c = openDB()
+# def addLocation(lat, long):
+#     conn, c = openDB()
+#
+#     insert = ([float(lat), float(long)])
+#     c.execute("""INSERT INTO Locations VALUES(?,?)""", insert)
+#
+#     conn.commit()
+#     conn.close()
 
-    insert = ([float(lat), float(long)])
-    c.execute("""INSERT INTO Locations VALUES(?,?)""", insert)
 
-    conn.commit()
-    conn.close()
-
-def getLocations():
-    conn,c = openDB()
-    c.execute("""SELECT * FROM Locations""")
-    locations = c.fetchall()
-
-    df = pd.DataFrame(columns="Longitude Latitude".split())
-    for i in range(len(locations)):
-        df.loc[i] = locations[i]
-    return df.to_json(orient='records')
-
-    conn.commit()
-    conn.close()
 ################################################################################
 # MAIN
 
-if __name__=="__main__":
+if __name__== "__main__":
 
     conn,c = openDB()
 
     c.execute("""DROP TABLE IF EXISTS Locations""")
 
     c.execute("""CREATE TABLE Locations (
-    lat numeric PRIMARY KEY,
+    lat numeric,
     long numeric
     )""")
 
     conn.commit()
     conn.close()
-
-    addLocation(2,3.5)
-    addLocation(4,5)
+    #
+    # addLocation(2,3.5)
+    # addLocation(4,5)
 
 
 ################################################################################
@@ -57,14 +46,42 @@ if __name__=="__main__":
 
     @app.route("/", methods=['GET', 'POST'])
     def main():
-        if request.method == 'POST':
-                lat = request.document.getElementById('pointLat')
-                addLocation(lat, 9999)
-                return render_template('templates/json.html')
         return render_template('map.html')
 
     @app.route('/locations')
     def printLoc():
         pass
+
+    @app.route('/api/locations', methods=['GET'])
+    def getLocations():
+        conn, c = openDB()
+        c.execute("""SELECT * FROM Locations""")
+        locations = c.fetchall()
+
+        df = pd.DataFrame(columns="Longitude Latitude".split())
+        for i in range(len(locations)):
+            df.loc[i] = locations[i]
+
+        conn.commit()
+        conn.close()
+        return df.to_json(orient='records')
+
+
+    @app.route('/api/locations', methods=['POST'])
+    def addLocation():
+        content = request.json
+        print(content)
+        coordinates = request.json
+
+        conn, c = openDB()
+
+        for coordinate in coordinates:
+            test = coordinate["Latitude"]
+            insert = ([float(coordinate["Latitude"]), float(coordinate["Longitude"])])
+            c.execute("""INSERT INTO Locations VALUES(?,?)""", insert)
+
+        conn.commit()
+        conn.close()
+        return ('', 204)
 
     app.run(debug=True)
---
+++ Yohan/static/scripts/app.js
@@ -0,0 +1,8 @@
+function helloWorld() {
+    return "Hello World"
+}
+
+function addCoordinate(latitude, longitude) {
+    console.error(latutude);
+    console.error(longitude);
+}
